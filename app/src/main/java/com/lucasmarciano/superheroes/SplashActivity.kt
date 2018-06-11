768cf3eb7c5423ca60c6e158872d4b57ee3d4369
SERVIDORES\lucasmarciano <lucasmarciano@inatel.br>
2018-06-11 13:04:40 -0300

    first commit

---
+++ app/src/main/java/com/lucasmarciano/superheroes/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.lucasmarciano.superheroes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
